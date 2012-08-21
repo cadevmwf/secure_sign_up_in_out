4b9739fd747f9dd32455280429d3ae3c6ea54e27
Raghu Betina <rbetina@gmail.com>
2012-08-21 07:01:59 -0500

    Authenticate the user.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,15 @@ class SessionsController < ApplicationController
   end
   
   def create
+    @email = params[:email]
+    user = User.find_by_email(@email)
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user_url(user), :notice => "Signed in as #{@email}."
+    else
+      flash[:notice] = 'Sign-in unsuccessful.'
+      render 'new'
+    end
   end
   
   def destroy
